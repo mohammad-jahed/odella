930f346be680c12d3286520d23890fc3e78b35ab
mohammad-jahed <mohamed.jahed33@gmail.com>
2023-03-31 14:36:46 +0300

    Adding Trips to The System

---
+++ app/Models/Trip.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Trip extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'supervisor_id', 'bus_driver_id', 'time_id'
+    ];
+
+    public function times()
+    {
+        return $this->belongsToMany(Time::class);
+    }
+
+    public function supervisor()
+    {
+        return $this->belongsTo(User::class, 'id', 'supervisor_id');
+    }
+
+    public function bus_driver()
+    {
+        return $this->belongsTo(BusDriver::class, 'id', 'bus_driver_id');
+    }
+}
