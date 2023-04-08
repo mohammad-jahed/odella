fc70195a2dfb9f72a4942e98f7e564c2f5b2d0d4
nawar.maaz <nawar.maaz@smartsoft-co.com>
2023-04-08 20:52:58 +0300

    Confirm student attendance in trip.

---
+++ app/Http/Requests/Student/ConfirmAttendanceRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\Student;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+use JetBrains\PhpStorm\ArrayShape;
+
+class ConfirmAttendanceRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    #[ArrayShape(['confirmAttendance1' => "array", 'confirmAttendance2' => "array"])]
+    public function rules(): array
+    {
+        return [
+            //
+            'confirmAttendance1'=>['required','boolean',Rule::in([true,false])],
+            'confirmAttendance2'=>['required','boolean',Rule::in([true,false])],
+        ];
+    }
+}
