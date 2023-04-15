ba4ac120b6ff14b641cc0e3e92485a90eadba615
mohammad-jahed <mohamed.jahed33@gmail.com>
2023-04-15 09:17:23 +0300

    Moving Firebase Key To env File

--- config/larafirebase.php
+++ config/larafirebase.php
@@ -2,6 +2,7 @@
 
 return [
 
-    'authentication_key' => 'AAAAcbjRAu4:APA91bGQtr-1au9rH0BcvZXTJtDBvLpraGutWOYf8GF8zgFeXA3yprCIRq0PzkZ58tOTxMw0X4ZTrIN2kyvt-YZBLzdDlxwM41VsFnwiEtIK_a7XoOVu1GQFkYrzNTr38tViKKuKUSug'
+    'authentication_key' => env('FIREBASE_KEY')
+
 
 ];
