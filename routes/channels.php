c71b8b03f6314afbc76b1619b9ab063b8e067128
Nawar Maaz <n.maaz@smartsoft-co.com>
2023-06-12 23:47:16 +0300

    Some edits in channels.php

--- routes/channels.php
+++ routes/channels.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Broadcast;
 
 /*
@@ -7,16 +8,16 @@ use Illuminate\Support\Facades\Broadcast;
 | Broadcast Channels
 |--------------------------------------------------------------------------
 |
-| Here you may register all of the event broadcasting channels that your
+| Here you may register all the event broadcasting channels that your
 | application supports. The given channel authorization callbacks are
 | used to check if an authenticated user can listen to the channel.
 |
 */
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+    return (int)$user->id === (int)$id;
 });
 
-Broadcast::channel('tracking.{trip_id}',function ($user, $trip_id) {
-   return true;
+Broadcast::channel('tracking.{trip_id}', function (User $user, $trip_id) {
+    return !$user->trips()->where('id', $trip_id)->get()->isEmpty();
 });
