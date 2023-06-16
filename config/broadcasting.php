f41ce587bd0997f1711d50537f95058a04d6778d
mohammad-jahed <mohamed.jahed33@gmail.com>
2023-06-16 20:44:43 +0300

    Moving Back to Pusher

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,15 +37,15 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'encrypted' => false,
-                'host' => env('PUSHER_HOST'),
-                'port' => env('PUSHER_PORT'),
-                //'useTLS'=> true,
-                'scheme' => env('PUSHER_SCHEME'),
-                'curl_options' => [
-                    CURLOPT_SSL_VERIFYHOST => 0,
-                    CURLOPT_SSL_VERIFYPEER => 0,
-                ],
+//                'encrypted' => false,
+//                'host' => env('PUSHER_HOST'),
+//                'port' => env('PUSHER_PORT'),
+//                //'useTLS'=> true,
+//                'scheme' => env('PUSHER_SCHEME'),
+//                'curl_options' => [
+//                    CURLOPT_SSL_VERIFYHOST => 0,
+//                    CURLOPT_SSL_VERIFYPEER => 0,
+//                ],
             ],
             'client_options' => [
                 'verify' => false,
